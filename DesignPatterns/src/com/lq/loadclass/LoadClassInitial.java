161a28dfcdf13f86caad8e325f39dc0aeb507067
codinglq <704788249@qq.com>
2014-12-11 17:58:15 +0800

    画心.

--- DesignPatterns/src/com/lq/loadclass/LoadClassInitial.java
+++ DesignPatterns/src/com/lq/loadclass/LoadClassInitial.java
@@ -2,9 +2,12 @@ package com.lq.loadclass;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Collection;
 
 /**
  * javc使用class.forname方式加载类，这不就是c语言中的动态链接库是一个玩意吗？
+ * 动态加载类，配合着设计模式，然后就可以在不改变客户端代码的情况下就能添加新的功能，
+ * 做到了一套代码，无限扩充功能，差不多这样吧。
  * @author lq
  *
  */
@@ -13,7 +16,6 @@ public class LoadClassInitial {
 	public static void main(String[] args) {
 
 		Class c1 = String.class;
-		
 		String str = new String();
 		Class c2 = str.getClass();
 		
